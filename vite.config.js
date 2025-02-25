8b39574d8aef9e86667d789275e72e109446f380
igorC5 <igorcandidofs@gmail.com>
2025-02-25 18:26:00 -0300

    mudando base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "/Facilite",
+  base: "/Facilite",
   publicDir:'public',
   build: {
     outDir: 'dist',
